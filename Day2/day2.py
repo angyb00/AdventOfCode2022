b11ec379beadecb95ff2bc7bb5874f514fa6b9d9
Angarag Gansukh <angaraggansukh@csu.fullerton.edu>
2022-12-04 20:09:45 -0800

    Finished part 2.

--- Day2/day2.py
+++ Day2/day2.py
@@ -1,6 +1,5 @@
-#print("Part 1")
-#print("----------------------------------------------------------------")
-
+print("Part 1")
+print("----------------------------------------------------------------")
 
 """
 A -> Rock 
@@ -17,8 +16,18 @@ Win -> 6 pts
 
 9 Total Combinations
 """
-score_combination = {1: "B X", 4: "A X", 7: "C X", 2: "C Y", 5: "B Y", 
-8: "A Y", 3: "A Z", 6: "C Z", 9: "B Z"}
+# Look up table 
+score_combination = {
+1: "B X", 
+4: "A X",
+7: "C X",
+2: "C Y",
+5: "B Y",
+8: "A Y",
+3: "A Z",
+6: "C Z",
+9: "B Z"
+}
 
 
 def compute_score(match):   #return val only not list
@@ -38,8 +47,47 @@ print("----------------------------------------------------------------")
 print("Part 2")    
 print("----------------------------------------------------------------")
 
+"""
+X -> Lose 
+Y -> Draw 
+Z -> Win
+"""
+
+total_score = 0
+scores = {} 
+
+#Match requires Python 3.10 or newer
+#Create new commbinations based off the conditions and use our compute score function
+def create_combination(line):
+    condition = line[2]
+    opponent_choice = line[0]
+    player_choice = " " + line[2]
+    match condition:
+        case "X":
+            if opponent_choice == "A":
+                player_choice = " Z"
+            elif opponent_choice == "C":
+                player_choice = " Y"
+        case "Y":
+            if opponent_choice == "A":
+                player_choice = " X"
+            elif opponent_choice == "C":
+                player_choice = " Z"
+        case "Z":
+            if opponent_choice == "A":
+                player_choice = " Y"
+            elif opponent_choice == "C":
+                player_choice = " X"
+        case _:
+            return "Error"
+    return opponent_choice + player_choice
 
 
+with open("strategy.txt", "r") as strategies:
+    for strategy in strategies:
+        new_combo = create_combination(strategy.strip())
+        total_score += compute_score(new_combo)
+       
 
-
+print(f"Total score: {total_score}")
 print("----------------------------------------------------------------")
